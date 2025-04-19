aa51214d5d84ab80f119b06591b0820d36359806
opticayaan <ayaan.gill@me.com>
2025-04-19 12:32:53 -0400

    Added reusable header component, blank page for calculator, and ids to sampledata

---
+++ Main_WebsiteIteration_1/header.js
@@ -0,0 +1,22 @@
+const headerTemplate = document.createElement("template");
+
+headerTemplate.innerHTML = `
+    <div class="header">
+        <h1>EasyPantry</h1>
+        <div class="nav">
+            <a href="homepage.html" id="home">Home</a>
+            <a href="BrowseIngredients.html" id="ingredients">Browse Ingredients</a>
+            <a href="calculator.html" id="calculator">Substitution Calculator</a>
+        </div>
+    </div>
+`;
+
+function addHeader(activePage) {
+  headerTemplate.content.getElementById(activePage).classList.add("active");
+  const headerDiv = document.getElementById("header");
+  if (headerDiv) {
+    headerDiv.appendChild(headerTemplate.content);
+  } else {
+    console.error("Header div not found");
+  }
+}
--- README.md
+++ README.md
@@ -1,2 +1,16 @@
-# IDSN-531-Final-project
-Easy Pantry
+# IDSN-531-Final-project: Easy Pantry
+
+## Reusable Header
+
+A reusable header component is written in the `header.js` file. To include it on your page, add a `div` with `id="header"` to your page where you'd like the header to appear, and include the following script tags in the `<head>` tag of your page:
+
+```
+<script src="header.js"></script>
+<script>
+    document.addEventListener("DOMContentLoaded", function () {
+        addHeader("{NAVLINK-ID}");
+    });
+</script>
+```
+
+Make sure to replace `{NAVLINK-ID}` with the id of the link to your page in the `header.js` file.
