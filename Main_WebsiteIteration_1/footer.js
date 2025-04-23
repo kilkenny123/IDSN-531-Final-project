b301d1a94679642a3ecdbb45bdc6faa55a1acedb
opticayaan <ayaan.gill@me.com>
2025-04-23 17:06:42 -0400

    Mobile styling

--- Main_WebsiteIteration_1/footer.js
+++ Main_WebsiteIteration_1/footer.js
@@ -4,36 +4,41 @@ footerTemplate.innerHTML = `
     <div class="footer">
         <div class="footer-content">
           <img src="logo-white.png" alt="Logo" id="logo"></img>
-          <div class="footer-social">
-            <span>Follow Us:</span>
-            <a href="#" aria-label="Instagram"
-              ><img
-                width="24"
-                height="24"
-                src="https://img.icons8.com/ios/24/instagram-new--v1.png"
-                alt="instagram-new--v1"
-            /></a>
-            <a href="#" aria-label="TikTok"
-              ><img
-                width="24"
-                height="24"
-                src="https://img.icons8.com/material-outlined/24/tiktok.png"
-                alt="tiktok"
-            /></a>
-            <a href="#" aria-label="Pinterest"
-              ><img
-                width="24"
-                height="24"
-                src="https://img.icons8.com/ios/24/pinterest--v1.png"
-                alt="pinterest--v1"
-            /></a>
-            <a href="#" aria-label="Tumblr"
-              ><img
-                width="24"
-                height="24"
-                src="https://img.icons8.com/ios/24/tumblr--v1.png"
-                alt="tumblr--v1"
-            /></a>
+          <div class="footer-right">
+            <div class="footer-links">
+              <a href="contact.html">Contact Us</a>
+            </div>
+            <div class="footer-social">
+              <span>Follow Us:</span>
+              <a href="#" aria-label="Instagram"
+                ><img
+                  width="24"
+                  height="24"
+                  src="https://img.icons8.com/ios/24/instagram-new--v1.png"
+                  alt="instagram-new--v1"
+              /></a>
+              <a href="#" aria-label="TikTok"
+                ><img
+                  width="24"
+                  height="24"
+                  src="https://img.icons8.com/material-outlined/24/tiktok.png"
+                  alt="tiktok"
+              /></a>
+              <a href="#" aria-label="Pinterest"
+                ><img
+                  width="24"
+                  height="24"
+                  src="https://img.icons8.com/ios/24/pinterest--v1.png"
+                  alt="pinterest--v1"
+              /></a>
+              <a href="#" aria-label="Tumblr"
+                ><img
+                  width="24"
+                  height="24"
+                  src="https://img.icons8.com/ios/24/tumblr--v1.png"
+                  alt="tumblr--v1"
+              /></a>
+            </div>
           </div>
         </div>
     </div>
--- Main_WebsiteIteration_1/header.js
+++ Main_WebsiteIteration_1/header.js
@@ -4,10 +4,13 @@ headerTemplate.innerHTML = `
     <div class="header">
         <div class="header-content">
           <img src="logo-white.png" alt="Logo" id="logo"></img>
+          <div class="nav-icon">
+            <img src="menu-icon.png" alt="Menu Icon" id="menu-icon"></img>
+          </div>
           <div class="nav">
-              <a href="homepage.html" id="home">Home</a>
-              <a href="BrowseIngredients.html" id="ingredients">Browse Ingredients</a>
-              <a href="calculator.html" id="calculator">Substitution Calculator</a>
+            <a href="homepage.html" id="home">Home</a>
+            <a href="BrowseIngredients.html" id="ingredients">Browse Ingredients</a>
+            <a href="calculator.html" id="calculator">Substitution Calculator</a>
             <a href="RequestPage.html" id="request">Request Form</a>
             <a href="about.html" id="about">About Us</a>
           </div>
@@ -16,7 +19,10 @@ headerTemplate.innerHTML = `
 `;
 
 function addHeader(activePage) {
-  headerTemplate.content.getElementById(activePage).classList.add("active");
+  const activeNavlink = headerTemplate.content.getElementById(activePage);
+  if (activeNavlink) {
+    activeNavlink.classList.add("active");
+  }
   const headerDiv = document.getElementById("header");
   if (headerDiv) {
     headerDiv.appendChild(headerTemplate.content);
@@ -24,3 +30,12 @@ function addHeader(activePage) {
     console.error("Header div not found");
   }
 }
+
+$(document).ready(function () {
+  const menuIcon = document.getElementById("menu-icon");
+  const nav = document.querySelector(".nav");
+
+  menuIcon.addEventListener("click", function () {
+    nav.classList.toggle("active");
+  });
+});
