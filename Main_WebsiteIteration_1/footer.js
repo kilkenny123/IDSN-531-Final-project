394284ec965ad08ae7c67c7e6e30de3c8e342dfe
mahi-prs <mahi.prs@gmail.com>
2025-04-23 14:53:02 -0700

    Updated Footer Icons

--- Main_WebsiteIteration_1/footer.js
+++ Main_WebsiteIteration_1/footer.js
@@ -11,33 +11,13 @@ footerTemplate.innerHTML = `
             <div class="footer-social">
               <span>Follow Us:</span>
               <a href="#" aria-label="Instagram"
-                ><img
-                  width="24"
-                  height="24"
-                  src="https://img.icons8.com/ios/24/instagram-new--v1.png"
-                  alt="instagram-new--v1"
-              /></a>
+                ><svg  xmlns="http://www.w3.org/2000/svg"  width="24"  height="24"  viewBox="0 0 24 24"  fill="none"  stroke="white"  stroke-width="2"  stroke-linecap="round"  stroke-linejoin="round"  class="icon icon-tabler icons-tabler-outline icon-tabler-brand-instagram"><path stroke="none" d="M0 0h24v24H0z" fill="none"/><path d="M4 8a4 4 0 0 1 4 -4h8a4 4 0 0 1 4 4v8a4 4 0 0 1 -4 4h-8a4 4 0 0 1 -4 -4z" /><path d="M9 12a3 3 0 1 0 6 0a3 3 0 0 0 -6 0" /><path d="M16.5 7.5v.01" /></svg></a>
               <a href="#" aria-label="TikTok"
-                ><img
-                  width="24"
-                  height="24"
-                  src="https://img.icons8.com/material-outlined/24/tiktok.png"
-                  alt="tiktok"
-              /></a>
+                ><svg  xmlns="http://www.w3.org/2000/svg"  width="24"  height="24"  viewBox="0 0 24 24"  fill="none"  stroke="white"  stroke-width="2"  stroke-linecap="round"  stroke-linejoin="round"  class="icon icon-tabler icons-tabler-outline icon-tabler-brand-tiktok"><path stroke="none" d="M0 0h24v24H0z" fill="none"/><path d="M21 7.917v4.034a9.948 9.948 0 0 1 -5 -1.951v4.5a6.5 6.5 0 1 1 -8 -6.326v4.326a2.5 2.5 0 1 0 4 2v-11.5h4.083a6.005 6.005 0 0 0 4.917 4.917z" /></svg></a>
               <a href="#" aria-label="Pinterest"
-                ><img
-                  width="24"
-                  height="24"
-                  src="https://img.icons8.com/ios/24/pinterest--v1.png"
-                  alt="pinterest--v1"
-              /></a>
+                ><svg  xmlns="http://www.w3.org/2000/svg"  width="24"  height="24"  viewBox="0 0 24 24"  fill="none"  stroke="currentColor"  stroke-width="2"  stroke-linecap="round"  stroke-linejoin="round"  class="icon icon-tabler icons-tabler-outline icon-tabler-brand-pinterest"><path stroke="none" d="M0 0h24v24H0z" fill="none"/><path d="M8 20l4 -9" /><path d="M10.7 14c.437 1.263 1.43 2 2.55 2c2.071 0 3.75 -1.554 3.75 -4a5 5 0 1 0 -9.7 1.7" /><path d="M12 12m-9 0a9 9 0 1 0 18 0a9 9 0 1 0 -18 0" /></svg></a>
               <a href="#" aria-label="Tumblr"
-                ><img
-                  width="24"
-                  height="24"
-                  src="https://img.icons8.com/ios/24/tumblr--v1.png"
-                  alt="tumblr--v1"
-              /></a>
+                ><svg  xmlns="http://www.w3.org/2000/svg"  width="24"  height="24"  viewBox="0 0 24 24"  fill="none"  stroke="currentColor"  stroke-width="2"  stroke-linecap="round"  stroke-linejoin="round"  class="icon icon-tabler icons-tabler-outline icon-tabler-brand-tumblr"><path stroke="none" d="M0 0h24v24H0z" fill="none"/><path d="M14 21h4v-4h-4v-6h4v-4h-4v-4h-4v1a3 3 0 0 1 -3 3h-1v4h4v6a4 4 0 0 0 4 4" /></svg></a>
             </div>
           </div>
         </div>
